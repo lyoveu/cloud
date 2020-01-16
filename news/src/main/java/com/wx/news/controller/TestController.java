ae3920246db1a5f8c1956a0ab1cf54e4925ea3aa
lyoveu <1228080240@qq.com>
2020-01-16 14:13:10 +0800

    config

---
+++ news/src/main/java/com/wx/news/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.wx.news.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${wx.name}")
+    private String name;
+    @Value("${wx.msg}")
+    private String message;
+
+    @RequestMapping("/test")
+    public String test(){
+        return name + "---" + message;
+    }
+}
